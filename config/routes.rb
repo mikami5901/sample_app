af9aef44e6b5a97bbcc6b73cf3594e867e678f1e
Tsubasa Mikami <tsumi6838@gmail.com>
2023-12-27 21:38:18 +0900

    c6

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get 'signup', to: 'users#new'
-  
+  root 'static_pages#home'
+  get  '/signup', to: 'users#new'
   get  '/home', to: 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   get  '/about', to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'static_pages#home'
+
 end
