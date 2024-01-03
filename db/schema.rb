cf629368c712d84d35519501bf6e7d50f7b05392
Tsubasa Mikami <tsumi6838@gmail.com>
2024-01-03 22:28:34 +0900

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_30_130853) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_122228) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_30_130853) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
